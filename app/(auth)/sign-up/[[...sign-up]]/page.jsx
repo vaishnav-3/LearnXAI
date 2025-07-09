a5350100fdc8ca7f1e19ae606d40eb163b1f349b
Vaishnav-3 <vaishnavpatil003@gmail.com>
2025-07-09 10:42:18 +0530

    implement sign-in and sign-up pages

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,23 @@
+import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="relative bg-white flex min-h-screen w-full items-center justify-center overflow-hidden">
+
+      <div className="flex z-50 flex-col items-center">
+        <div className="flex items-center gap-2 mb-6">
+          <Image
+            src="/logo.svg"
+            alt="LearnX-AI Logo"
+            width={50}
+            height={50}
+            className="object-contain"
+          />
+          <h1 className="text-3xl z-50 font-bold text-black">LearnX-AI</h1>
+        </div>
+        <SignUp />
+      </div>
+    </div>
+  );
+}
