28715050ec79c55473c3dc188c70d450748385c2
Vaishnav-3 <vaishnavpatil003@gmail.com>
2025-07-23 20:14:49 +0530

    add provider setup

---
+++ app/provider.js
@@ -0,0 +1,42 @@
+"use client";
+
+import { db } from "../configs/db";
+import { USER_TABLE } from "../configs/schema";
+import { useUser } from "@clerk/nextjs";
+import axios from "axios";
+import { eq } from "drizzle-orm";
+import React, { useEffect } from "react";
+
+function Provider({ children }) {
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (user) {
+      CheckIsNewUser();
+    }
+  }, []);
+
+  const CheckIsNewUser = async () => {
+    const result = await db
+      .select()
+      .from(USER_TABLE)
+      .where(eq(USER_TABLE.email, user?.primaryEmailAddress?.emailAddress));
+    if (result?.length == 0) {
+      const userResp = await db
+        .insert(USER_TABLE)
+        .values({
+          name: user?.fullName,
+          email: user?.primaryEmailAddress?.emailAddress,
+        })
+        .returning({ id: USER_TABLE.id });
+
+    }
+
+    const resp = await axios.post('/app/api/create-user', {user: user})
+
+
+  };
+  return <div>{children}</div>;
+}
+
+export default Provider;
